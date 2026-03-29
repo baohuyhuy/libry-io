80180f11fd7e7d65b3d02fbfc36c2a1418125204
23120131 <pbhuy16072005@gmail.com>
2026-03-29 16:07:29 +0700

    initialize project structure with Spring Boot, MySQL, and Docker support

---
+++ src/main/java/io/libry/LibryIoApplication.java
@@ -0,0 +1,13 @@
+package io.libry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibryIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibryIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/libry/LibryIoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.libry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibryIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
